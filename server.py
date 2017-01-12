8c1ff6abd588097bb1d7be93da4a0d527f8d1360
Matt Pedersen <matthewnpedersen@gmail.com>
2017-01-11 19:45:42 -0800

    begin flask code

--- server.py
+++ server.py
@@ -5,7 +5,25 @@ app.secret_key = 'asecretkey'
 
 @app.route('/')
 def index():
+  if not 'gold_count' in session:
+    session['gold_count'] = 0
   return render_template('index.html')
+
+@app.route('/process_money', methods=['POST'])
+def process_money():
+  building_chosen = request.form['building']
+
+  # if farm +10-20 gold
+  if building_chosen == 'farm':
+
+  # if cave +5-10 gold
+  elif building_chosen == 'cave':
+  # if house +2-5 gold
+  elif building_chosen == 'house':
+  # if casino +/-50 gold
+  elif building_chosen == 'casino':
+
+  return redirect('/')
 
 app.run(debug=True)
 
